37a1d5e7c6b25c96d9b007d53eea59cf4ddf7be7
Sophia Brown <sb911718@ohio.edu>
2022-01-13 21:25:39 -0500

    added opening file and count number of lines

--- filesize.cc
+++ filesize.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 #include <fstream>
 using namespace std;
 
@@ -7,12 +9,26 @@ using namespace std;
 //program: ./filesize
 // input.3lines: 3
 // input.1line: 1
-
+//
 int main( int argc, char* argv[] )
 {
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    std::cout << "program: " << argv[0] << '\n' ;
+    for (int arg = 1; arg < argc; ++arg){ //for parsing the command line arguments 
+        ifstream instream;
+        string dataline;
+        int lineCount = 0;
+        instream.open(argv[arg], std::ifstream::in);
+        if (!(instream.fail())){
+            while(!instream.eof()){
+                getline(instream, dataline); //getline of data and increment 
+                lineCount++;
+            }
+        }
+        else{ //if it is not a readable file that can't be opened then set line count to -1
+            lineCount = -1;
+        }
+        instream.close();       
+        std::cout << " " << argv[arg] << ": " << lineCount << '\n' ;
+    }
     exit(0); // this means that the program executed correctly!
 }
